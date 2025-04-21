a8ec7dca521eec971720881ed6b5b033a1ba8fdd
Christiaan de Ridder <christiaan@channelengine.com>
2025-04-21 16:46:36 +0200

    Use file-scoped namespace declarations

--- Usenet/Nntp/Parsers/HeaderDateParser.cs
+++ Usenet/Nntp/Parsers/HeaderDateParser.cs
@@ -1,138 +1,136 @@
-﻿using System;
-using System.Globalization;
+﻿using System.Globalization;
 using System.Text.RegularExpressions;
 
-namespace Usenet.Nntp.Parsers
+namespace Usenet.Nntp.Parsers;
+
+internal static class HeaderDateParser
 {
-    internal static class HeaderDateParser
+    private const string _dateTimeRegexString =
+        @"(?:\s*"
+        + @"(?<dayName>Sun|Mon|Tue|Wed|Thu|Fri|Sat),)?\s*"
+        + @"(?<day>\d{1,2})\s+"
+        + @"(?<month>Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)\s+"
+        + @"(?<year>\d{2,4})\s+"
+        + @"(?<hour>\d{1,2}):(?<min>\d{1,2})(?::(?<sec>\d{1,2}))?\s*"
+        + @"(?<tz>[+-]\d+|(?:UT|UTC|GMT|Z|EDT|EST|CDT|CST|MDT|MST|PDT|PST|A|N|M|Y|[A-Z]+)"
+        + @")?";
+
+    private static readonly Regex _dateTimeRegex = new Regex(_dateTimeRegexString, RegexOptions.IgnoreCase | RegexOptions.Compiled);
+
+    /// <summary>
+    /// Parses header date/time strings as described in the
+    /// <a href="https://tools.ietf.org/html/rfc5322#section-3.3">Date and Time Specification</a>.
+    /// </summary>
+    /// <param name="value"></param>
+    /// <returns></returns>
+    internal static DateTimeOffset? Parse(string value)
     {
-        private const string _dateTimeRegexString =
-              @"(?:\s*"
-            + @"(?<dayName>Sun|Mon|Tue|Wed|Thu|Fri|Sat),)?\s*"
-            + @"(?<day>\d{1,2})\s+"
-            + @"(?<month>Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)\s+"
-            + @"(?<year>\d{2,4})\s+"
-            + @"(?<hour>\d{1,2}):(?<min>\d{1,2})(?::(?<sec>\d{1,2}))?\s*"
-            + @"(?<tz>[+-]\d+|(?:UT|UTC|GMT|Z|EDT|EST|CDT|CST|MDT|MST|PDT|PST|A|N|M|Y|[A-Z]+)"
-            + @")?";
-
-        private static readonly Regex _dateTimeRegex = new Regex(_dateTimeRegexString, RegexOptions.IgnoreCase | RegexOptions.Compiled);
-
-        /// <summary>
-        /// Parses header date/time strings as described in the
-        /// <a href="https://tools.ietf.org/html/rfc5322#section-3.3">Date and Time Specification</a>.
-        /// </summary>
-        /// <param name="value"></param>
-        /// <returns></returns>
-        internal static DateTimeOffset? Parse(string value)
+        if (string.IsNullOrWhiteSpace(value))
         {
-            if (string.IsNullOrWhiteSpace(value))
-            {
-                return null;
-            }
-
-            var matches = _dateTimeRegex.Match(value);
-            if (!matches.Success)
-            {
-                throw new FormatException(Resources.Nntp.BadHeaderDateFormat);
-            }
-
-            var day = int.Parse(matches.Groups["day"].Value, CultureInfo.InvariantCulture);
-            var month = matches.Groups["month"].Value;
-            var year = int.Parse(matches.Groups["year"].Value, CultureInfo.InvariantCulture);
-            var hour = int.Parse(matches.Groups["hour"].Value, CultureInfo.InvariantCulture);
-            var minute = int.Parse(matches.Groups["min"].Value, CultureInfo.InvariantCulture);
-            _ = int.TryParse(matches.Groups["sec"].Value, out var second);
-            var tz = matches.Groups["tz"].Value;
-            var zone = ParseZone(tz);
-
-            int monthIndex = 1 + Array.FindIndex(DateTimeFormatInfo.InvariantInfo.AbbreviatedMonthNames,
-                m => string.Equals(m, month, StringComparison.OrdinalIgnoreCase));
-
-            if (matches.Groups["year"].Value.Length < 4)
-            {
-                year += GetCentury(year, monthIndex, day) * 100;
-            }
-
-            return new DateTimeOffset(year, monthIndex, day, hour, minute, second, 0, zone);
+            return null;
         }
 
-        private static int GetCentury(int year, int month, int day)
+        var matches = _dateTimeRegex.Match(value);
+        if (!matches.Success)
         {
-            DateTime today = DateTime.UtcNow.Date;
-            int currentCentury = today.Year / 100;
-            return new DateTime(currentCentury * 100 + year, month, day, 0, 0, 0, DateTimeKind.Utc) > today
-                ? currentCentury - 1
-                : currentCentury;
+            throw new FormatException(Resources.Nntp.BadHeaderDateFormat);
         }
 
-        private static TimeSpan ParseZone(string value)
+        var day = int.Parse(matches.Groups["day"].Value, CultureInfo.InvariantCulture);
+        var month = matches.Groups["month"].Value;
+        var year = int.Parse(matches.Groups["year"].Value, CultureInfo.InvariantCulture);
+        var hour = int.Parse(matches.Groups["hour"].Value, CultureInfo.InvariantCulture);
+        var minute = int.Parse(matches.Groups["min"].Value, CultureInfo.InvariantCulture);
+        _ = int.TryParse(matches.Groups["sec"].Value, out var second);
+        var tz = matches.Groups["tz"].Value;
+        var zone = ParseZone(tz);
+
+        int monthIndex = 1 + Array.FindIndex(DateTimeFormatInfo.InvariantInfo.AbbreviatedMonthNames,
+            m => string.Equals(m, month, StringComparison.OrdinalIgnoreCase));
+
+        if (matches.Groups["year"].Value.Length < 4)
         {
-            // The time zone must be as specified in RFC822, https://tools.ietf.org/html/rfc822#section-5
-
-            if (!short.TryParse(value, out short zone))
-            {
-                switch (value)
-                {
-                    // UTC and empty are not specified in RFC822, but allowing them since they are commonly used
-                    case "UTC":
-                    case "UT":
-                    case "GMT":
-                    case "Z":
-                    case "":
-                        break;
-
-                    case "EDT":
-                        zone = -0400;
-                        break;
-
-                    case "EST":
-                    case "CDT":
-                        zone = -0500;
-                        break;
-
-                    case "CST":
-                    case "MDT":
-                        zone = -0600;
-                        break;
-
-                    case "MST":
-                    case "PDT":
-                        zone = -0700;
-                        break;
-
-                    case "PST":
-                        zone = -0800;
-                        break;
-
-                    case "A":
-                        zone = -0100;
-                        break;
-
-                    case "N":
-                        zone = +0100;
-                        break;
-
-                    case "M":
-                        zone = -1200;
-                        break;
-
-                    case "Y":
-                        zone = +1200;
-                        break;
-
-                    default:
-                        throw new FormatException(Resources.Nntp.BadHeaderDateFormat);
-                }
-            }
-            else if (-9999 > zone || zone > 9999)
-            {
-                throw new FormatException(Resources.Nntp.BadHeaderDateFormat);
-            }
-
-            int minute = zone % 100;
-            int hour = zone / 100;
-            return TimeSpan.FromMinutes(hour * 60 + minute);
+            year += GetCentury(year, monthIndex, day) * 100;
         }
+
+        return new DateTimeOffset(year, monthIndex, day, hour, minute, second, 0, zone);
+    }
+
+    private static int GetCentury(int year, int month, int day)
+    {
+        DateTime today = DateTime.UtcNow.Date;
+        int currentCentury = today.Year / 100;
+        return new DateTime(currentCentury * 100 + year, month, day, 0, 0, 0, DateTimeKind.Utc) > today
+            ? currentCentury - 1
+            : currentCentury;
+    }
+
+    private static TimeSpan ParseZone(string value)
+    {
+        // The time zone must be as specified in RFC822, https://tools.ietf.org/html/rfc822#section-5
+
+        if (!short.TryParse(value, out short zone))
+        {
+            switch (value)
+            {
+                // UTC and empty are not specified in RFC822, but allowing them since they are commonly used
+                case "UTC":
+                case "UT":
+                case "GMT":
+                case "Z":
+                case "":
+                    break;
+
+                case "EDT":
+                    zone = -0400;
+                    break;
+
+                case "EST":
+                case "CDT":
+                    zone = -0500;
+                    break;
+
+                case "CST":
+                case "MDT":
+                    zone = -0600;
+                    break;
+
+                case "MST":
+                case "PDT":
+                    zone = -0700;
+                    break;
+
+                case "PST":
+                    zone = -0800;
+                    break;
+
+                case "A":
+                    zone = -0100;
+                    break;
+
+                case "N":
+                    zone = +0100;
+                    break;
+
+                case "M":
+                    zone = -1200;
+                    break;
+
+                case "Y":
+                    zone = +1200;
+                    break;
+
+                default:
+                    throw new FormatException(Resources.Nntp.BadHeaderDateFormat);
+            }
+        }
+        else if (-9999 > zone || zone > 9999)
+        {
+            throw new FormatException(Resources.Nntp.BadHeaderDateFormat);
+        }
+
+        int minute = zone % 100;
+        int hour = zone / 100;
+        return TimeSpan.FromMinutes(hour * 60 + minute);
     }
 }
--- UsenetTests/Nntp/Parsers/HeaderDateParserTests.cs
+++ UsenetTests/Nntp/Parsers/HeaderDateParserTests.cs
@@ -1,120 +1,117 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Globalization;
+﻿using System.Globalization;
 using Usenet.Nntp.Parsers;
 using Xunit;
 
-namespace UsenetTests.Nntp.Parsers
+namespace UsenetTests.Nntp.Parsers;
+
+public class HeaderDateParserTests
 {
-    public class HeaderDateParserTests
-    {
-        public static readonly IEnumerable<object[]> ParseData =
-        [
-            ["Mon, 1 May 2017 1:55", new DateTimeOffset(2017, 5, 1, 1, 55, 0, TimeSpan.Zero)],
-            ["1 May 2017 1:55:33", new DateTimeOffset(2017, 5, 1, 1, 55, 33, TimeSpan.Zero)],
-            ["01 May 2017 13:55", new DateTimeOffset(2017, 5, 1, 13, 55, 0, TimeSpan.Zero)],
-            ["01 May 2017 13:55:33", new DateTimeOffset(2017, 5, 1, 13, 55, 33, TimeSpan.Zero)],
-            ["01 May 2017 13:55:33 +0000", new DateTimeOffset(2017, 5, 1, 13, 55, 33, TimeSpan.Zero)],
-            ["01 May 2017 13:55:33 -0000", new DateTimeOffset(2017, 5, 1, 13, 55, 33, TimeSpan.Zero)],
-            ["01 May 2017 13:55:33 +0000 (UTC)", new DateTimeOffset(2017, 5, 1, 13, 55, 33, TimeSpan.Zero)],
-            ["01 May 2017 13:55:33 -0000 (UTC)", new DateTimeOffset(2017, 5, 1, 13, 55, 33, TimeSpan.Zero)],
-            ["01 May 2017 13:55:33 +0100", new DateTimeOffset(2017, 5, 1, 13, 55, 33, TimeSpan.FromHours(1))],
-            ["01 May 2017 13:55:33 -0100", new DateTimeOffset(2017, 5, 1, 13, 55, 33, TimeSpan.FromHours(-1))],
+    public static readonly IEnumerable<object[]> ParseData =
+    [
+        ["Mon, 1 May 2017 1:55", new DateTimeOffset(2017, 5, 1, 1, 55, 0, TimeSpan.Zero)],
+        ["1 May 2017 1:55:33", new DateTimeOffset(2017, 5, 1, 1, 55, 33, TimeSpan.Zero)],
+        ["01 May 2017 13:55", new DateTimeOffset(2017, 5, 1, 13, 55, 0, TimeSpan.Zero)],
+        ["01 May 2017 13:55:33", new DateTimeOffset(2017, 5, 1, 13, 55, 33, TimeSpan.Zero)],
+        ["01 May 2017 13:55:33 +0000", new DateTimeOffset(2017, 5, 1, 13, 55, 33, TimeSpan.Zero)],
+        ["01 May 2017 13:55:33 -0000", new DateTimeOffset(2017, 5, 1, 13, 55, 33, TimeSpan.Zero)],
+        ["01 May 2017 13:55:33 +0000 (UTC)", new DateTimeOffset(2017, 5, 1, 13, 55, 33, TimeSpan.Zero)],
+        ["01 May 2017 13:55:33 -0000 (UTC)", new DateTimeOffset(2017, 5, 1, 13, 55, 33, TimeSpan.Zero)],
+        ["01 May 2017 13:55:33 +0100", new DateTimeOffset(2017, 5, 1, 13, 55, 33, TimeSpan.FromHours(1))],
+        ["01 May 2017 13:55:33 -0100", new DateTimeOffset(2017, 5, 1, 13, 55, 33, TimeSpan.FromHours(-1))],
 
-            ["01 May 2017 13:55 +1030", new DateTimeOffset(2017, 5, 1, 13, 55, 0, TimeSpan.FromMinutes(10 * 60 + 30))],
-            ["01 May 2017 13:55 -1030", new DateTimeOffset(2017, 5, 1, 13, 55, 0, -TimeSpan.FromMinutes(10 * 60 + 30))],
+        ["01 May 2017 13:55 +1030", new DateTimeOffset(2017, 5, 1, 13, 55, 0, TimeSpan.FromMinutes(10 * 60 + 30))],
+        ["01 May 2017 13:55 -1030", new DateTimeOffset(2017, 5, 1, 13, 55, 0, -TimeSpan.FromMinutes(10 * 60 + 30))],
 
-            ["01 May 2017 13:55+1030", new DateTimeOffset(2017, 5, 1, 13, 55, 0, TimeSpan.FromMinutes(10 * 60 + 30))],
-            ["01 May 2017 13:55-1030", new DateTimeOffset(2017, 5, 1, 13, 55, 0, -TimeSpan.FromMinutes(10 * 60 + 30))],
+        ["01 May 2017 13:55+1030", new DateTimeOffset(2017, 5, 1, 13, 55, 0, TimeSpan.FromMinutes(10 * 60 + 30))],
+        ["01 May 2017 13:55-1030", new DateTimeOffset(2017, 5, 1, 13, 55, 0, -TimeSpan.FromMinutes(10 * 60 + 30))],
             
-            ["1 Jan 2017 00:00:00 +0000", new DateTimeOffset(2017, 1, 1, 0, 0, 0, TimeSpan.Zero)],
-            ["1 Feb 2017 00:00:00 +0000", new DateTimeOffset(2017, 2, 1, 0, 0, 0, TimeSpan.Zero)],
-            ["1 Mar 2017 00:00:00 +0000", new DateTimeOffset(2017, 3, 1, 0, 0, 0, TimeSpan.Zero)],
-            ["1 Apr 2017 00:00:00 +0000", new DateTimeOffset(2017, 4, 1, 0, 0, 0, TimeSpan.Zero)],
-            ["1 May 2017 00:00:00 +0000", new DateTimeOffset(2017, 5, 1, 0, 0, 0, TimeSpan.Zero)],
-            ["1 Jun 2017 00:00:00 +0000", new DateTimeOffset(2017, 6, 1, 0, 0, 0, TimeSpan.Zero)],
-            ["1 Jul 2017 00:00:00 +0000", new DateTimeOffset(2017, 7, 1, 0, 0, 0, TimeSpan.Zero)],
-            ["1 Aug 2017 00:00:00 +0000", new DateTimeOffset(2017, 8, 1, 0, 0, 0, TimeSpan.Zero)],
-            ["1 Sep 2017 00:00:00 +0000", new DateTimeOffset(2017, 9, 1, 0, 0, 0, TimeSpan.Zero)],
-            ["1 Oct 2017 00:00:00 +0000", new DateTimeOffset(2017, 10, 1, 0, 0, 0, TimeSpan.Zero)],
-            ["1 Nov 2017 00:00:00 +0000", new DateTimeOffset(2017, 11, 1, 0, 0, 0, TimeSpan.Zero)],
-            ["1 Dec 2017 00:00:00 +0000", new DateTimeOffset(2017, 12, 1, 0, 0, 0, TimeSpan.Zero)],
+        ["1 Jan 2017 00:00:00 +0000", new DateTimeOffset(2017, 1, 1, 0, 0, 0, TimeSpan.Zero)],
+        ["1 Feb 2017 00:00:00 +0000", new DateTimeOffset(2017, 2, 1, 0, 0, 0, TimeSpan.Zero)],
+        ["1 Mar 2017 00:00:00 +0000", new DateTimeOffset(2017, 3, 1, 0, 0, 0, TimeSpan.Zero)],
+        ["1 Apr 2017 00:00:00 +0000", new DateTimeOffset(2017, 4, 1, 0, 0, 0, TimeSpan.Zero)],
+        ["1 May 2017 00:00:00 +0000", new DateTimeOffset(2017, 5, 1, 0, 0, 0, TimeSpan.Zero)],
+        ["1 Jun 2017 00:00:00 +0000", new DateTimeOffset(2017, 6, 1, 0, 0, 0, TimeSpan.Zero)],
+        ["1 Jul 2017 00:00:00 +0000", new DateTimeOffset(2017, 7, 1, 0, 0, 0, TimeSpan.Zero)],
+        ["1 Aug 2017 00:00:00 +0000", new DateTimeOffset(2017, 8, 1, 0, 0, 0, TimeSpan.Zero)],
+        ["1 Sep 2017 00:00:00 +0000", new DateTimeOffset(2017, 9, 1, 0, 0, 0, TimeSpan.Zero)],
+        ["1 Oct 2017 00:00:00 +0000", new DateTimeOffset(2017, 10, 1, 0, 0, 0, TimeSpan.Zero)],
+        ["1 Nov 2017 00:00:00 +0000", new DateTimeOffset(2017, 11, 1, 0, 0, 0, TimeSpan.Zero)],
+        ["1 Dec 2017 00:00:00 +0000", new DateTimeOffset(2017, 12, 1, 0, 0, 0, TimeSpan.Zero)],
 
-            // Valid timezone formats
-            ["1 Jan 2020 00:00:00 UTC", new DateTimeOffset(2020, 1, 1, 0, 0, 0, TimeSpan.Zero)],
-            ["1 Jan 2020 00:00:00 UT", new DateTimeOffset(2020, 1, 1, 0, 0, 0, TimeSpan.Zero)],
-            ["1 Jan 2020 00:00:00 GMT", new DateTimeOffset(2020, 1, 1, 0, 0, 0, TimeSpan.Zero)],
-            ["1 Jan 2020 00:00:00 Z", new DateTimeOffset(2020, 1, 1, 0, 0, 0, TimeSpan.Zero)],
-            ["1 Jan 2020 00:00:00 EDT", new DateTimeOffset(2020, 1, 1, 0, 0, 0, TimeSpan.FromHours(-04))],
-            ["1 Jan 2020 00:00:00 EST", new DateTimeOffset(2020, 1, 1, 0, 0, 0, TimeSpan.FromHours(-05))],
-            ["1 Jan 2020 00:00:00 CDT", new DateTimeOffset(2020, 1, 1, 0, 0, 0, TimeSpan.FromHours(-05))],
-            ["1 Jan 2020 00:00:00 CST", new DateTimeOffset(2020, 1, 1, 0, 0, 0, TimeSpan.FromHours(-06))],
-            ["1 Jan 2020 00:00:00 MDT", new DateTimeOffset(2020, 1, 1, 0, 0, 0, TimeSpan.FromHours(-06))],
-            ["1 Jan 2020 00:00:00 MST", new DateTimeOffset(2020, 1, 1, 0, 0, 0, TimeSpan.FromHours(-07))],
-            ["1 Jan 2020 00:00:00 PDT", new DateTimeOffset(2020, 1, 1, 0, 0, 0, TimeSpan.FromHours(-07))],
-            ["1 Jan 2020 00:00:00 PST", new DateTimeOffset(2020, 1, 1, 0, 0, 0, TimeSpan.FromHours(-08))],
-            ["1 Jan 2020 00:00:00 A", new DateTimeOffset(2020, 1, 1, 0, 0, 0, TimeSpan.FromHours(-01))],
-            ["1 Jan 2020 00:00:00 N", new DateTimeOffset(2020, 1, 1, 0, 0, 0, TimeSpan.FromHours(+01))],
-            ["1 Jan 2020 00:00:00 M", new DateTimeOffset(2020, 1, 1, 0, 0, 0, TimeSpan.FromHours(-12))]
-        ];
+        // Valid timezone formats
+        ["1 Jan 2020 00:00:00 UTC", new DateTimeOffset(2020, 1, 1, 0, 0, 0, TimeSpan.Zero)],
+        ["1 Jan 2020 00:00:00 UT", new DateTimeOffset(2020, 1, 1, 0, 0, 0, TimeSpan.Zero)],
+        ["1 Jan 2020 00:00:00 GMT", new DateTimeOffset(2020, 1, 1, 0, 0, 0, TimeSpan.Zero)],
+        ["1 Jan 2020 00:00:00 Z", new DateTimeOffset(2020, 1, 1, 0, 0, 0, TimeSpan.Zero)],
+        ["1 Jan 2020 00:00:00 EDT", new DateTimeOffset(2020, 1, 1, 0, 0, 0, TimeSpan.FromHours(-04))],
+        ["1 Jan 2020 00:00:00 EST", new DateTimeOffset(2020, 1, 1, 0, 0, 0, TimeSpan.FromHours(-05))],
+        ["1 Jan 2020 00:00:00 CDT", new DateTimeOffset(2020, 1, 1, 0, 0, 0, TimeSpan.FromHours(-05))],
+        ["1 Jan 2020 00:00:00 CST", new DateTimeOffset(2020, 1, 1, 0, 0, 0, TimeSpan.FromHours(-06))],
+        ["1 Jan 2020 00:00:00 MDT", new DateTimeOffset(2020, 1, 1, 0, 0, 0, TimeSpan.FromHours(-06))],
+        ["1 Jan 2020 00:00:00 MST", new DateTimeOffset(2020, 1, 1, 0, 0, 0, TimeSpan.FromHours(-07))],
+        ["1 Jan 2020 00:00:00 PDT", new DateTimeOffset(2020, 1, 1, 0, 0, 0, TimeSpan.FromHours(-07))],
+        ["1 Jan 2020 00:00:00 PST", new DateTimeOffset(2020, 1, 1, 0, 0, 0, TimeSpan.FromHours(-08))],
+        ["1 Jan 2020 00:00:00 A", new DateTimeOffset(2020, 1, 1, 0, 0, 0, TimeSpan.FromHours(-01))],
+        ["1 Jan 2020 00:00:00 N", new DateTimeOffset(2020, 1, 1, 0, 0, 0, TimeSpan.FromHours(+01))],
+        ["1 Jan 2020 00:00:00 M", new DateTimeOffset(2020, 1, 1, 0, 0, 0, TimeSpan.FromHours(-12))]
+    ];
 
-        public static readonly IEnumerable<object[]> TimezoneParseFailureData =
-        [
-            ["1 Jan 2020 00:00:00 BAD", typeof(FormatException)],
-            ["1 Jan 2020 00:00:00 -10000", typeof(FormatException)],
-            ["1 Jan 2020 00:00:00 +10000", typeof(FormatException)]
-        ];
+    public static readonly IEnumerable<object[]> TimezoneParseFailureData =
+    [
+        ["1 Jan 2020 00:00:00 BAD", typeof(FormatException)],
+        ["1 Jan 2020 00:00:00 -10000", typeof(FormatException)],
+        ["1 Jan 2020 00:00:00 +10000", typeof(FormatException)]
+    ];
 
-        public static readonly IEnumerable<object[]> CenturyData =
-        [
-            ["01 May 16 13:55:33 +0000", new DateTimeOffset(2016, 5, 1, 13, 55, 33, TimeSpan.Zero)],
-            ["01 May 99 13:55:33 +0000", new DateTimeOffset(1999, 5, 1, 13, 55, 33, TimeSpan.Zero)]
-        ];
+    public static readonly IEnumerable<object[]> CenturyData =
+    [
+        ["01 May 16 13:55:33 +0000", new DateTimeOffset(2016, 5, 1, 13, 55, 33, TimeSpan.Zero)],
+        ["01 May 99 13:55:33 +0000", new DateTimeOffset(1999, 5, 1, 13, 55, 33, TimeSpan.Zero)]
+    ];
 
-        [Theory]
-        [MemberData(nameof(ParseData))]
-        internal void HeaderDateShouldBeParsedCorrectly(string headerDate, DateTimeOffset expectedDateTime)
-        {
-            DateTimeOffset? actualDateTime = HeaderDateParser.Parse(headerDate);
-            Assert.Equal(expectedDateTime, actualDateTime);
-        }
-
-        [Theory]
-        [MemberData(nameof(TimezoneParseFailureData))]
-        internal void HeaderDateShouldNotBeParsedCorrectly(string headerDate, Type exceptionType)
-        {
-            Assert.Throws(exceptionType, () => HeaderDateParser.Parse(headerDate));
-        }
-
-        [Fact]
-        internal void ObsoleteTwoDigitYearBeforeCurrentDateShouldBeParsedCorrectly()
-        {
-            DateTimeOffset yesterday = new(DateTime.UtcNow.Date.AddDays(-1), TimeSpan.Zero);
-            DateTimeOffset expectedDate = yesterday;
-            string headerValue = yesterday.ToString("dd MMM yy HH:mm:ss", CultureInfo.InvariantCulture) + " +0000";
-            DateTimeOffset actualDate = HeaderDateParser.Parse(headerValue).GetValueOrDefault();
-            Assert.Equal(expectedDate, actualDate);
-        }
-
-        [Fact]
-        internal void ObsoleteTwoDigitYearAfterCurrentDateShouldBeParsedCorrectly()
-        {
-            DateTimeOffset tomorrow = new(DateTime.UtcNow.Date.AddDays(+1), TimeSpan.Zero);
-            DateTimeOffset expectedDate = tomorrow.AddYears(-100);
-            string headerValue = tomorrow.ToString("dd MMM yy HH:mm:ss", CultureInfo.InvariantCulture) + " +0000";
-            DateTimeOffset actualDate = HeaderDateParser.Parse(headerValue).GetValueOrDefault();
-            Assert.Equal(expectedDate, actualDate);
-        }
-
-        [Fact]
-        internal void ObsoleteTwoDigitYearOnCurrentDateShouldBeParsedCorrectly()
-        {
-            var today = new DateTimeOffset(DateTime.UtcNow.Date, TimeSpan.Zero);
-            DateTimeOffset expectedDate = today;
-            string headerValue = expectedDate.ToString("dd MMM yy HH:mm:ss", CultureInfo.InvariantCulture) + " +0000";
-            DateTimeOffset actualDate = HeaderDateParser.Parse(headerValue).GetValueOrDefault();
-            Assert.Equal(expectedDate, actualDate);
-        }
-
+    [Theory]
+    [MemberData(nameof(ParseData))]
+    internal void HeaderDateShouldBeParsedCorrectly(string headerDate, DateTimeOffset expectedDateTime)
+    {
+        DateTimeOffset? actualDateTime = HeaderDateParser.Parse(headerDate);
+        Assert.Equal(expectedDateTime, actualDateTime);
     }
+
+    [Theory]
+    [MemberData(nameof(TimezoneParseFailureData))]
+    internal void HeaderDateShouldNotBeParsedCorrectly(string headerDate, Type exceptionType)
+    {
+        Assert.Throws(exceptionType, () => HeaderDateParser.Parse(headerDate));
+    }
+
+    [Fact]
+    internal void ObsoleteTwoDigitYearBeforeCurrentDateShouldBeParsedCorrectly()
+    {
+        DateTimeOffset yesterday = new(DateTime.UtcNow.Date.AddDays(-1), TimeSpan.Zero);
+        DateTimeOffset expectedDate = yesterday;
+        string headerValue = yesterday.ToString("dd MMM yy HH:mm:ss", CultureInfo.InvariantCulture) + " +0000";
+        DateTimeOffset actualDate = HeaderDateParser.Parse(headerValue).GetValueOrDefault();
+        Assert.Equal(expectedDate, actualDate);
+    }
+
+    [Fact]
+    internal void ObsoleteTwoDigitYearAfterCurrentDateShouldBeParsedCorrectly()
+    {
+        DateTimeOffset tomorrow = new(DateTime.UtcNow.Date.AddDays(+1), TimeSpan.Zero);
+        DateTimeOffset expectedDate = tomorrow.AddYears(-100);
+        string headerValue = tomorrow.ToString("dd MMM yy HH:mm:ss", CultureInfo.InvariantCulture) + " +0000";
+        DateTimeOffset actualDate = HeaderDateParser.Parse(headerValue).GetValueOrDefault();
+        Assert.Equal(expectedDate, actualDate);
+    }
+
+    [Fact]
+    internal void ObsoleteTwoDigitYearOnCurrentDateShouldBeParsedCorrectly()
+    {
+        var today = new DateTimeOffset(DateTime.UtcNow.Date, TimeSpan.Zero);
+        DateTimeOffset expectedDate = today;
+        string headerValue = expectedDate.ToString("dd MMM yy HH:mm:ss", CultureInfo.InvariantCulture) + " +0000";
+        DateTimeOffset actualDate = HeaderDateParser.Parse(headerValue).GetValueOrDefault();
+        Assert.Equal(expectedDate, actualDate);
+    }
+
 }
