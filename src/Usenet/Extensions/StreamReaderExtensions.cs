f0f0e2b29c8a5ec7a234bdef811ffb976d50c590
Christiaan de Ridder <christiaan@channelengine.com>
2025-12-07 14:33:14 +0100

    Add compatibility shim for cancelable ReadLineAsync

--- src/Usenet/Extensions/StreamReaderExtensions.cs
+++ src/Usenet/Extensions/StreamReaderExtensions.cs
@@ -11,5 +11,15 @@ internal static class StreamReaderExtensions
         return reader.ReadToEndAsync();
 #endif
     }
+
+    public static Task<string> ReadLineAsync(this StreamReader reader, CancellationToken cancellationToken)
+    {
+#if NET7_0_OR_GREATER
+        return reader.ReadLineAsync(cancellationToken);
+#else
+        cancellationToken.ThrowIfCancellationRequested();
+        return reader.ReadLineAsync();
+#endif
+    }
 }
 
