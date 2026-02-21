50a3b26661274518a249c21b4ed148f2d042b3d8
Christiaan de Ridder <christiaan@channelengine.com>
2026-02-21 15:28:34 +0100

    Update IEqualityComparer to use nullable reference types

--- src/Usenet/Util/MultiSetComparer.cs
+++ src/Usenet/Util/MultiSetComparer.cs
@@ -6,7 +6,7 @@
 /// (<a href="https://stackoverflow.com/questions/50098/comparing-two-collections-for-equality-irrespective-of-the-order-of-items-in-the">Source</a>)
 /// </summary>
 /// <typeparam name="T"></typeparam>
-internal class MultiSetComparer<T> : IEqualityComparer<IEnumerable<T>>
+internal class MultiSetComparer<T> : IEqualityComparer<IEnumerable<T>> where T : notnull
 {
     private readonly IEqualityComparer<T> _comparer;
 
@@ -35,38 +35,23 @@ internal class MultiSetComparer<T> : IEqualityComparer<IEnumerable<T>>
     /// <param name="first"></param>
     /// <param name="second"></param>
     /// <returns></returns>
-    public bool Equals(IEnumerable<T> first, IEnumerable<T> second)
+    public bool Equals(IEnumerable<T>? first, IEnumerable<T>? second)
     {
-        if (first == null)
-        {
-            return second == null;
-        }
+        if (first == null) return second == null;
+        if (second == null) return false;
 
-        if (second == null)
-        {
-            return false;
-        }
-
-        if (ReferenceEquals(first, second))
-        {
+        if (ReferenceEquals(first, second)) 
             return true;
-        }
 
         if (first is not ICollection<T> firstCollection ||
             second is not ICollection<T> secondCollection)
-        {
             return !HaveMismatchedElement(first, second);
-        }
 
         if (firstCollection.Count != secondCollection.Count)
-        {
             return false;
-        }
 
         if (firstCollection.Count == 0)
-        {
             return true;
-        }
 
         return !HaveMismatchedElement(first, second);
     }
@@ -94,7 +79,7 @@ internal class MultiSetComparer<T> : IEqualityComparer<IEnumerable<T>>
         return false;
     }
 
-    private Dictionary<T, int> GetElementCounts(IEnumerable<T> enumerable, out int nullCount)
+    private Dictionary<T, int> GetElementCounts(IEnumerable<T?> enumerable, out int nullCount)
     {
         nullCount = 0;
         var dictionary = new Dictionary<T, int>(_comparer);
